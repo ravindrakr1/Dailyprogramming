63610666dce2341a193190cf9fde1b40a7ce18f1
ravindrakr1 <ravindra.cse.tu@gmail.com>
2022-09-13 22:59:21 +0530

    checkin

--- src/stream/Stream1.java
+++ src/stream/Stream1.java
@@ -9,15 +9,15 @@ public class Stream1 {
     public static void main(String[] args) {
         //stream
         //convert list to map
-        List<String> listnames = Arrays.asList("Shilpi","Ravindra","Tanu");
-        List<String> listnames1 = new ArrayList<>();
-        listnames1.add("Shilpi");
+        List<String> listnames = Arrays.asList("Shilpi","Shilpi","Tanu");
+//        List<String> listnames1 = new ArrayList<>();
+//        listnames1.add("Shilpi");
 //        listnames.add("Shilpi");
 //        System.out.println(listnames);
 //        listnames.stream().filter(p->p.contains("S")).map(str -> str.length()).collect(Collectors.toList());
 //        listnames1.stream().map(Str -> Str+"new").collect(Collectors.toList());
-
-        System.out.println(listnames1.stream().map(Str -> Str+"new").collect(Collectors.toList()));
+        System.out.println(listnames.stream().map(Str -> Str+"new").collect(Collectors.toMap(p -> p.length(),p -> p,(s,a)-> a)));
+        System.out.println(listnames.stream().map(Str -> Str+"new").collect(Collectors.toList()));
 
         //
 
